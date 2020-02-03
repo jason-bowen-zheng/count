7b5a02087ec7b0f491a9a48d6bbbe47e44997771
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-02-03 19:18:43 +0800

    Add files via upload

--- funclib.py
+++ funclib.py
@@ -2,34 +2,56 @@
 # function library
 
 import math
+import random
 
 # Math function #
 
-def abs(x):
+def math_abs(x):
     # abs(x)
     if isinstance(x, (int, float)):
         return {'NUM': math.fabs(x)}
     else:
         return {'ERR': 'value error'}
 
-def ceil(x):
+def math_ceil(x):
     # ceil(x)
     if isinstance(x, (int, float)):
         return {'NUM': math.ceil(x)}
     else:
         return {'ERR': 'value error'}
 
-def exp(x):
+def math_cos(x):
+    # cos(x)
+    if isinstance(x, (int, float)):
+        return {'NUM': math.cos(x)}
+    else:
+        return {'ERR': 'value error'}
+
+def math_exp(x):
     # exp(x)
     if isinstance(x, (int, float)):
         return {'NUM': math.exp(x)}
     else:
         return {'ERR': 'value error'}
 
-def floor(x):
+def math_floor(x):
     # floor(x)
     if isinstance(x, (int, float)):
         return {'NUM': math.floor(x)}
+    else:
+        return {'ERR': 'value error'}
+
+def math_round(x, n=None):
+    # round(x, n)
+    if isinstance(x, (int, float)) and (isinstance(n, int) or n == None):
+        return {'NUM': round(x, n)}
+    else:
+        return {'ERR': 'value error'}
+
+def sin(x):
+    # sin(x)
+    if isinstance(x, (int, float)):
+        return {'NUM': math.sin(x)}
     else:
         return {'ERR': 'value error'}
 
@@ -39,3 +61,9 @@ def sqrt(x):
         return {'NUM': math.sqrt(x)}
     else:
         return {'ERR': 'value error'}
+
+def tan(x):
+    if isinstance(x, (int, float)):
+        return {'NUM': math.tan(x)}
+    else:
+        return {'ERROR': 'value error'}
