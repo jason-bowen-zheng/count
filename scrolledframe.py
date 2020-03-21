935cd0d26b2eb8c22ae50ddbcc6d86c4d37492b1
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-03-21 19:19:19 +0800

    Add files via upload

--- scrolledframe.py
+++ scrolledframe.py
@@ -40,6 +40,29 @@ class ScrolledFrame(ttk.Frame):
         canvas.bind('<Configure>', _configure_canvas)
         
 
+class ScrolledText(tk.Text):
+    def __init__(self, master=None, **kw):
+        self.frame = ttk.Frame(master)
+        self.vbar = ttk.Scrollbar(self.frame)
+        self.vbar.pack(side='right', fill='y')
+
+        kw.update({'yscrollcommand': self.vbar.set})
+        tk.Text.__init__(self, self.frame, **kw)
+        self.pack(side='left', fill='both', expand=1)
+        self.vbar['command'] = self.yview
+        # Copy geometry methods of self.frame without overriding Text
+        text_meths = vars(tk.Text).keys()
+        methods = vars(tk.Pack).keys() | vars(tk.Grid).keys() | vars(tk.Place).keys()
+        methods = methods.difference(text_meths)
+
+        for m in methods:
+            if m[0] != '_' and m != 'config' and m != 'configure':
+                setattr(self, m, getattr(self.frame, m))
+
+    def __str__(self):
+        return str(self.frame)
+
+
 class VerticalScrolledFrame(ttk.Frame):
     'idlelib.configdialog.VerticalScrolledFrame'
     def __init__(self, parent, *args, **kw):
