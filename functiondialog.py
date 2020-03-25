6038b8df74b8e3258b6945dcc6ba9bbf82fa2665
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-03-25 19:10:13 +0800

    Add files via upload

--- functiondialog.py
+++ functiondialog.py
@@ -22,14 +22,18 @@ class FunctionDialog():
                                   'Statistics',
                                   'Text')
         self.typebox.bind('<<ComboboxSelected>>', self.typeboxsel)
+        self.typebox.set('All')
         self.frame = ttk.Frame(self.functiondialog, width=300)
         self.scrollbar = ttk.Scrollbar(self.frame)
         self.listbox = tk.Listbox(self.frame, height=8,
                                   yscrollcommand=self.scrollbar.set)
-        self.listbox.bind('<<ListBoxSelect>>', self.listboxsel)
+        self.listbox.bind('<Double-1>', self.listboxsel)
+        for k, v in function.functions.items():
+                self.listbox.insert('end', k)
         self.scrollbar.config(command=self.listbox.yview)
         self.labeldetail = ttk.Label(self.functiondialog, text='Detail:')
         self.detail = ScrolledText(self.functiondialog, height=5)
+        self.detail['state'] == 'disabled'
         self.detail.pack(side='bottom', fill='x', pady=3)
         self.labeldetail.pack(side='bottom', pady=1)
         self.frame.pack(side='bottom', fill='both')
@@ -46,21 +50,27 @@ class FunctionDialog():
                 self.listbox.insert('end', k)
         else:
             for k, v in function.functions.items():
-                print(v.__doc__.split('\n')[0])
                 if v.__doc__.split('\n')[0] == types + ' function':
                     self.listbox.insert('end', k)
 
     def listboxsel(self, event):
-        f = self.listbox.curselection()
+        if self.listbox.curselection() == ():
+            f = self.listbox.get(0)
+        else:
+            f = self.listbox.get(self.listbox.curselection()[0])
         for k, v in function.functions.items():
             if k == f:
                 self.detail['state'] = 'normal'
-                detail = '\n'.join(v.__doc__.split('\n', 2)[1:])
-                self.detail.delete('1.0', 'end')
-                self.detail.insert('end', detail)
+                if hasattr(v, '__doc__'):
+                    detail = v.__doc__.split('\n',2)[-1]
+                    self.detail.delete('1.0', 'end')
+                    self.detail.insert('end', detail)
                 self.detail['state'] = 'disable'
     
     def show(self):
+        self.typebox.set('All')
+        for f in function.functions.keys():
+            self.listbox.insert('end', f)
         self.functiondialog.geometry('300x270')
         self.functiondialog.resizable(False, False)
         self.functiondialog.mainloop()
