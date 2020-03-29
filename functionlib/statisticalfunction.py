0a4502fa90b062f065b4a359fea329bdc00ab26e
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-03-29 09:47:44 +0800

    Add files via upload

--- functionlib/statisticalfunction.py
+++ functionlib/statisticalfunction.py
@@ -1,5 +1,7 @@
 # xSheets/functionlib/statisticalfunction.py
 
+import statistics
+
 def statistical_average(seq):
     '''Statistical function
 list
@@ -10,7 +12,45 @@ return average number of seq.'''
         if x:
             l.append(x)
     else:
-        return static.fmean(l)
+        return statistics.fmean(l)
+
+def statistical_geometric_mean(seq):
+    '''Statistical function
+list
+geometric_mean(seq)
+return geometric mean of seq.'''
+    l = []
+    for x in seq:
+        if x:
+            l.append(x)
+    else:
+        return statistics.geometric_mean(l)
+
+def statistical_harmonic_mean(seq):
+    '''Statistical function
+list
+harmonic_mean(seq)
+return harmonic mean of seq.'''
+
+    l = []
+    for x in seq:
+        if x:
+            l.append(x)
+    else:
+        return statistics.harmonic_mean(l)
+
+def statistical_median(seq):
+    '''Statistical function
+list
+geometric_mean(seq)
+return median of seq.'''
+
+    l = []
+    for x in seq:
+        if x:
+            l.append(x)
+    else:
+        return statistics.median(l)
 
 def statistical_sum(seq):
     '''Statistical function
@@ -25,6 +65,9 @@ return sum of seq'''
         return total
 
 functions = {
-    'average':  statistical_average,
-    'sum':      statistical_sum
+    'average':          statistical_average,
+    'geometric_mean':   statistical_geometric_mean,
+    'harmonic_mean':    statistical_harmonic_mean,
+    'median':           statistical_median,
+    'sum':              statistical_sum
     }
