0921e39d14b57ec1c3bdee47e4f51883bd0dabfc
jason-bowen-zheng <58848782+jason-bowen-zheng@users.noreply.github.com>
2020-04-01 07:11:15 +0800

    Add files via upload

--- xscript.py
+++ xscript.py
@@ -10,7 +10,7 @@ class XscriptInterpreter():
     def __init__(self, string='', var={}, debug=False):
         self.restart(string, var, debug)
 
-    def exitrun(self, code=0):
+    def exit(self, code=0):
         print('\nProgram raise exit code: %s' % code)
     
     def restart(self, string='', var={}, debug=False):
@@ -30,7 +30,7 @@ class XscriptInterpreter():
             if self.now + 1 <= len(self.program):
                 line = self.program[self.now - 1]
             else:
-                self.exitrun()
+                self.exit()
                 break
             lines = shlex.split(line)
             ret = None
@@ -38,33 +38,39 @@ class XscriptInterpreter():
                 print('command:', lines)
             else:
                 pass
-            if lines == []:
-                pass
-            elif lines[0][0] == '#':
-                pass
-            elif lines[0] == 'exit':
-                self.exitrun(*lines[1:])
+            try:
+                if lines == []:
+                    pass
+                elif lines[0][0] == '#':
+                    pass
+                elif lines[0] == 'exit':
+                    self.exit(*lines[1:])
+                    break
+                elif lines[0] == 'puts':
+                    ret = self.puts(*lines[1:])
+                elif lines[0] == 'set':
+                    ret = self.set(*lines[1:])
+                elif lines[0] == 'unset':
+                    ret = self.unset(*lines[1:])
+                else:
+                    print('Error:%d:Unknow command: %s' % (self.now, lines[0]))
+                    self.exit(1)
+                    break
+            except Exception as err:
+                print('Error:%d:' % self.now, str(err))
+                self.exit(1)
                 break
-            elif lines[0] == 'puts':
-                ret = self.puts(*lines[1:])
-            elif lines[0] == 'set':
-                ret = self.set(*lines[1:])
-            elif lines[0] == 'unset':
-                ret = self.unset(*lines[1:])
             else:
-                print('Unknow command')
-                self.exitrun(1)
-                break
-            self.now += 1
-            if self.debug:
-                print('return:', ret)
-            else:
-                pass
-            if ret == None:
-                pass
-            elif ret[:6] == 'error ':
-                print('Error:', ret[6:])
-                break
+                self.now += 1
+                if self.debug:
+                    print('return:', ret)
+                else:
+                    pass
+                if ret == None:
+                    pass
+                elif ret[:6] == 'error ':
+                    print('Error:%d:' % self.now, ret[6:])
+                    break
         print('\nRunning in %ss' % (time.time() - starttime))
 
     def puts(self, *args):
@@ -82,7 +88,6 @@ class XscriptInterpreter():
                 return 'error Unknow name: %s' % name
 
 code = '''puts Hello
-set a b
 '''
 ipr = XscriptInterpreter(code, debug=0)
 ipr.run()
